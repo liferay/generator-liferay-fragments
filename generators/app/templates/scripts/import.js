2c925e917da6c4ac125b0166c4e090d577f98f8f
pavel-savinov <pavel.savinov@liferay.com>
2018-11-27 16:27:56 +0100

    LPS-87860 Use common scripts instead of internal functions

--- generators/app/templates/scripts/import.js
+++ generators/app/templates/scripts/import.js
@@ -1,58 +1,15 @@
 const chalk = require('chalk');
 const fs = require('fs');
 const glob = require('glob');
-const {log, logError, logNewLine, logIndent} = require('./log');
+const {logError, logNewLine, logIndent} = require('./log');
 const path = require('path');
-const readline = require('readline');
+const {askPortalData, getGroupId, getHost, getPassword, getUsername} = require('./portal');
+const readline = require('readline-sync');
 const request = require('request');
-
-const reader = readline.createInterface({
-	input: process.stdin,
-	output: process.stdout
-});
 
 const objectsInProcess = {};
 
-let host = 'http://localhost:8080',
-    username = 'test@liferay.com',
-    password = 'test',
-    companyId = 20099,
-    groupId = 20126;
-
-_askHost = () => {
-	reader.question(
-		`Liferay host & port? [${host}] `,
-		(answer) => {
-			host = answer !== '' ? answer : host;
-
-			_askUsername();
-		}
-	);
-};
-
-_askPassword = () => {
-	reader.question(
-		`Liferay administrator password? [${password}] `,
-		(answer) => {
-			password = answer !== '' ?  answer : password;
-
-			_requestCompanies();
-		}
-	);
-};
-
-_askUsername = () => {
-	reader.question(
-		`Liferay administrator username? [${username}] `,
-		(answer) => {
-			username = answer !== '' ?  answer : username;
-
-			_askPassword();
-		}
-	);
-};
-
-_deployCollections = () => {
+_importCollections = () => {
 	glob.sync(`${__dirname}/../src/*/collection.json`)
 		.map(collectionJSON => path.resolve(`${collectionJSON}/..`))
 		.forEach(collectionDirectory => {
@@ -61,26 +18,26 @@ _deployCollections = () => {
 
 				const collectionKey = path.basename(collectionDirectory);
 
-				logNewLine(`Deploying collection ${chalk.reset(collectionJSON.name)}`);
-
 				objectsInProcess[`collection${collectionJSON.name}`] = true;
 
 				request.post(
 					{
 						auth: {
-							user: username,
-							pass: password
+							user: getUsername(),
+							pass: getPassword()
 						},
 						formData: {
-							groupId: groupId,
+							groupId: getGroupId(),
 							fragmentCollectionKey: collectionKey,
 							name: collectionJSON.name,
 							description: collectionJSON.description
 						},
 						jar: true,
-						url: `${host}/api/jsonws/fragment.fragmentcollection/add-fragment-collection`
+						url: `${getHost()}/api/jsonws/fragment.fragmentcollection/add-fragment-collection`
 					},
 					(error, response, body) => {
+						logNewLine(`Deploying collection ${chalk.reset(collectionJSON.name)}`);
+
 						if (error || (response && response.statusCode != 200)) {
 							if (response.body && response.body.lastIndexOf("DuplicateFragmentCollectionKeyException") !== -1) {
 								_updateCollection(collectionJSON, collectionKey, collectionDirectory);
@@ -94,7 +51,7 @@ _deployCollections = () => {
 						else {
 							const collection = JSON.parse(body);
 
-							_deployFragments(
+							_importFragments(
 								collectionDirectory,
 								collection,
 								false
@@ -106,7 +63,7 @@ _deployCollections = () => {
 		);
 };
 
-_deployFragments = (collectionDirectory, collection, updateAll) => {
+_importFragments = (collectionDirectory, collection, updateAll) => {
 	glob.sync(`${collectionDirectory}/*/fragment.json`)
 		.map(fragmentJSON => path.resolve(`${fragmentJSON}/..`))
 		.forEach(fragmentDirectory => {
@@ -128,11 +85,11 @@ _deployFragments = (collectionDirectory, collection, updateAll) => {
 				request.post(
 					{
 						auth: {
-							user: username,
-							pass: password
+							user: getUsername(),
+							pass: getPassword()
 						},
 						formData: {
-							groupId: groupId,
+							groupId: getGroupId(),
 							fragmentCollectionId: collection.fragmentCollectionId,
 							fragmentEntryKey: fragmentKey,
 							name: fragmentJSON.name,
@@ -142,7 +99,7 @@ _deployFragments = (collectionDirectory, collection, updateAll) => {
 							status: 0
 						},
 						jar: true,
-						url: `${host}/api/jsonws/fragment.fragmententry/add-fragment-entry`
+						url: `${getHost()}/api/jsonws/fragment.fragmententry/add-fragment-entry`
 					},
 					(error, response, body) => {
 						if (error || (response && response.statusCode != 200)) {
@@ -166,11 +123,11 @@ _deployFragments = (collectionDirectory, collection, updateAll) => {
 
 _getCollection = (name, key, callback) => {
 	request.get(
-		`${host}/api/jsonws/fragment.fragmentcollection/get-fragment-collections/group-id/${groupId}/name/${name}/start/-1/end/-1/-order-by-comparator`,
+		`${getHost()}/api/jsonws/fragment.fragmentcollection/get-fragment-collections/group-id/${getGroupId()}/name/${name}/start/-1/end/-1/-order-by-comparator`,
 		{
 			auth: {
-				user: username,
-				pass: password
+				user: getUsername(),
+				pass: getPassword()
 			},
 			jar: true
 		},
@@ -196,11 +153,11 @@ _getCollection = (name, key, callback) => {
 
 _getFragment = (name, key, collectionId, callback) => {
 	request.get(
-		`${host}/api/jsonws/fragment.fragmententry/get-fragment-entries/group-id/${groupId}/fragment-collection-id/${collectionId}/name/${name}/status/0/start/-1/end/-1/-order-by-comparator`,
+		`${getHost()}/api/jsonws/fragment.fragmententry/get-fragment-entries/group-id/${getGroupId()}/fragment-collection-id/${collectionId}/name/${name}/status/0/start/-1/end/-1/-order-by-comparator`,
 		{
 			auth: {
-				user: username,
-				pass: password
+				user: getUsername(),
+				pass: getPassword()
 			},
 			jar: true
 		},
@@ -224,170 +181,96 @@ _getFragment = (name, key, collectionId, callback) => {
 	);
 };
 
-_requestCompanies = () => {
-	request.get(
-		`${host}/api/jsonws/company/get-companies`,
-		{
-			auth: {
-				user: username,
-				pass: password
-			},
-			jar: true
-		},
-		(error, response, body) => {
-			if (error || (response && response.statusCode != 200)) {
-				logError(`Error getting companies list from ${host}: ${error}, ${response}`);
-
-				process.exit(-1);
-			}
-
-			const companies = JSON.parse(body);
-
-			companies.forEach(
-				(company) => log(`${company.companyId} - ${company.webId}`)
-			);
-
-			reader.question(
-				`Company ID? [${companyId}] `,
-				(answer) => {
-					companyId = answer !== '' ? answer : companyId;
-
-					_requestGroups();
-				}
-			);
-		}
-	);
-};
-
-_requestGroups = () => {
-	request.get(
-		`${host}/api/jsonws/group/get-groups/company-id/${companyId}/parent-group-id/0/site/true`,
-		{
-			auth: {
-				user: username,
-				pass: password
-			},
-			jar: true
-		},
-		(error, response, body) => {
-			if (error || (response && response.statusCode != 200)) {
-				logError(`Error getting groups list from ${host}: ${error}, ${response}`);
-
-				process.exit(-1);
-			}
-
-			const groups = JSON.parse(body);
-
-			groups.forEach(
-				(group) => log(`${group.groupId} - ${group.friendlyURL}`)
-			);
-
-			reader.question(
-				`Group ID? [${groupId}] `,
-				(answer) => {
-					groupId = answer !== '' ? answer : groupId;
-
-					_deployCollections();
-				}
-			);
-
-		}
-	);
-};
-
 _updateCollection = (collectionJSON, collectionKey, collectionDirectory) => {
-	reader.question(
-		`Collection "${collectionJSON.name}" already exists, update it including all the fragments? [A/y/n] `,
-		(answer) => {
-			const updateFlag = (answer !== '' ? answer : 'A').toLowerCase();
+	var answer = readline.question(`Collection "${collectionJSON.name}" already exists, update it including all the fragments? [A/y/n] `);
 
-			switch (updateFlag.charAt(0)) {
-				case 'a':
-					_getCollection(
-						collectionJSON.name,
-						collectionKey,
-						(collection) => {
-							request.post(
-								{
-									auth: {
-										user: username,
-										pass: password
-									},
-									formData: {
-										groupId: groupId,
-										fragmentCollectionId: collection.fragmentCollectionId,
-										name: collectionJSON.name,
-										description: collectionJSON.description
-									},
-									jar: true,
-									url: `${host}/api/jsonws/fragment.fragmentcollection/update-fragment-collection`
-								},
-								(error, response, body) => {
-									if (error || (response && response.statusCode != 200)) {
-										logError(`Error updating collection ${chalk.reset(collectionJSON.name)}: ${error}, ${response}`);
+	const updateFlag = (answer !== '' ? answer : 'A').toLowerCase();
 
-										process.exit(-1);
-									}
+	switch (updateFlag.charAt(0)) {
+		case 'a':
+			_getCollection(
+				collectionJSON.name,
+				collectionKey,
+				(collection) => {
+					request.post(
+						{
+							auth: {
+								user: getUsername(),
+								pass: getPassword()
+							},
+							formData: {
+								groupId: getGroupId(),
+								fragmentCollectionId: collection.fragmentCollectionId,
+								name: collectionJSON.name,
+								description: collectionJSON.description
+							},
+							jar: true,
+							url: `${getHost()}/api/jsonws/fragment.fragmentcollection/update-fragment-collection`
+						},
+						(error, response, body) => {
+							if (error || (response && response.statusCode != 200)) {
+								logError(`Error updating collection ${chalk.reset(collectionJSON.name)}: ${error}, ${response}`);
 
-									const collection = JSON.parse(body);
+								process.exit(-1);
+							}
 
-									_deployFragments(
-										collectionDirectory,
-										collection,
-										true
-									);
-								}
+							const collection = JSON.parse(body);
+
+							_importFragments(
+								collectionDirectory,
+								collection,
+								true
 							);
-
 						}
 					);
-					break;
-				case 'y':
-					_getCollection(
-						collectionJSON.name,
-						collectionKey,
-						(collection) => {
-							request.post(
-								{
-									auth: {
-										user: username,
-										pass: password
-									},
-									formData: {
-										groupId: groupId,
-										fragmentCollectionId: collection.fragmentCollectionId,
-										name: collectionJSON.name,
-										description: collectionJSON.description
-									},
-									jar: true,
-									url: `${host}/api/jsonws/fragment.fragmentcollection/update-fragment-collection`
-								},
-								(error, response, body) => {
-									if (error || (response && response.statusCode != 200)) {
-										logError(`Error updating collection ${chalk.reset(collectionJSON.name)}: ${error}, ${response}`);
 
-										process.exit(-1);
-									}
+				}
+			);
+			break;
+		case 'y':
+			_getCollection(
+				collectionJSON.name,
+				collectionKey,
+				(collection) => {
+					request.post(
+						{
+							auth: {
+								user: getUsername(),
+								pass: getPassword()
+							},
+							formData: {
+								groupId: getGroupId(),
+								fragmentCollectionId: collection.fragmentCollectionId,
+								name: collectionJSON.name,
+								description: collectionJSON.description
+							},
+							jar: true,
+							url: `${getHost()}/api/jsonws/fragment.fragmentcollection/update-fragment-collection`
+						},
+						(error, response, body) => {
+							if (error || (response && response.statusCode != 200)) {
+								logError(`Error updating collection ${chalk.reset(collectionJSON.name)}: ${error}, ${response}`);
 
-									const collection = JSON.parse(body);
+								process.exit(-1);
+							}
 
-									_deployFragments(
-										collectionDirectory,
-										collection,
-										false
-									);
-								}
+							const collection = JSON.parse(body);
+
+							_importFragments(
+								collectionDirectory,
+								collection,
+								false
 							);
-
 						}
 					);
-					break;
-				default:
-					logNewLine(`Skipping existing collection ${chalk.reset(collectionJSON.name)}`);
-					break;
-			}
-		}
-	);
+
+				}
+			);
+			break;
+		default:
+			logNewLine(`Skipping existing collection ${chalk.reset(collectionJSON.name)}`);
+			break;
+	}
 };
 
 _updateFragment = (name, key, collectionId, css, html, js, updateAll) => {
@@ -395,11 +278,11 @@ _updateFragment = (name, key, collectionId, css, html, js, updateAll) => {
 		request.post(
 			{
 				auth: {
-					user: username,
-					pass: password
+					user: getUsername(),
+					pass: getPassword()
 				},
 				formData: {
-					groupId: groupId,
+					groupId: getGroupId(),
 					fragmentEntryId: fragment.fragmentEntryId,
 					name: name,
 					css: css,
@@ -408,7 +291,7 @@ _updateFragment = (name, key, collectionId, css, html, js, updateAll) => {
 					status: 0
 				},
 				jar: true,
-				url: `${host}/api/jsonws/fragment.fragmententry/update-fragment-entry`
+				url: `${getHost()}/api/jsonws/fragment.fragmententry/update-fragment-entry`
 			},
 			(error, response, body) => {
 				if (error || (response && response.statusCode != 200)) {
@@ -432,23 +315,20 @@ _updateFragment = (name, key, collectionId, css, html, js, updateAll) => {
 				doUpdateFragment(fragment);
 			}
 			else {
-				reader.question(
-					`Fragment "${name}" already exists, update it including all the fragments? [Y/n]`,
-					(answer) => {
-						const updateFlag = (answer !== '' ? answer : 'y').toLowerCase();
+				var answer = readline.question(`Fragment "${name}" already exists, update it including all the fragments? [Y/n]`);
 
-						switch (updateFlag.charAt(0)) {
-							case 'y':
-								doUpdateFragment(fragment);
-								break;
-							default:
-								logNewLine(`Skipping existing fragment ${chalk.reset(name)}`);
+				const updateFlag = (answer !== '' ? answer : 'y').toLowerCase();
 
-								_removeLock(`fragment${fragmentJSON.name}`);
-								break;
-						}
-					}
-				);
+				switch (updateFlag.charAt(0)) {
+					case 'y':
+						doUpdateFragment(fragment);
+						break;
+					default:
+						logNewLine(`Skipping existing fragment ${chalk.reset(name)}`);
+
+						_removeLock(`fragment${fragmentJSON.name}`);
+						break;
+				}
 			}
 		}
 	);
@@ -458,12 +338,11 @@ _removeLock = (lockName) => {
 	delete objectsInProcess[lockName];
 
 	if (Object.entries(objectsInProcess).length == 0) {
-		reader.close();
 		process.stdin.destroy();
 	}
 };
 
-_askHost();
+askPortalData(_importCollections);
 
 process.on(
 	'exit',
