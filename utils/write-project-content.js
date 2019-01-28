9c3b4f47ddfbaf1f225611e05a267f524b02d6bd
Pablo Molina <contact@pablomolina.me>
2019-01-28 12:55:26 +0100

    Add write project content function

---
+++ utils/write-project-content.js
@@ -0,0 +1,110 @@
+const fs = require('fs');
+const mkdirp = require('mkdirp');
+const path = require('path');
+const util = require('util');
+
+const writeFilePromise = util.promisify(fs.writeFile);
+
+/**
+ * @param {string} path File path
+ * @param {string} content New file content
+ */
+const _updateFile = async (path, content) => {
+  await writeFilePromise(path, content);
+};
+
+/**
+ * @param {string} path JSON file path
+ * @param {object} content JSON content
+ */
+const _updateJSON = async (path, content) => {
+  let newContent = content;
+
+  try {
+    const oldContent = require(path);
+
+    newContent = Object.assign({}, oldContent, content);
+  } catch (error) {}
+
+  await _updateFile(path, JSON.stringify(newContent, null, 2));
+};
+
+/**
+ * @param {string} fragmentBasePath Fragment base path
+ * @param {object} collection Collection data
+ * @param {object} fragment Fragment date
+ */
+const _writeProjectFragment = async (
+  fragmentBasePath,
+  collection,
+  fragment
+) => {
+  mkdirp.sync(fragmentBasePath);
+
+  await _updateJSON(
+    path.resolve(fragmentBasePath, 'fragment.json'),
+    fragment.metadata
+  );
+
+  await _updateFile(
+    path.resolve(fragmentBasePath, fragment.metadata.cssPath),
+    fragment.css
+  );
+
+  await _updateFile(
+    path.resolve(fragmentBasePath, fragment.metadata.htmlPath),
+    fragment.html
+  );
+
+  await _updateFile(
+    path.resolve(fragmentBasePath, fragment.metadata.jsPath),
+    fragment.js
+  );
+};
+
+/**
+ * @param {string} collectionBasePath Collection base path
+ * @param {object} collection Collection data
+ */
+const _writeProjectCollection = async (collectionBasePath, collection) => {
+  mkdirp.sync(path.resolve(collectionBasePath));
+
+  await _updateJSON(
+    path.resolve(collectionBasePath, 'collection.json'),
+    collection.metadata
+  );
+
+  await Promise.all(
+    collection.fragments.map(fragment =>
+      _writeProjectFragment(
+        path.resolve(collectionBasePath, fragment.slug),
+        collection,
+        fragment
+      )
+    )
+  );
+};
+
+/**
+ * @param {string} projectBasePath Project base path
+ * @param {object} projectContent Project data
+ */
+const writeProjectContent = async (projectBasePath, projectContent) => {
+  mkdirp.sync(path.resolve(projectBasePath, 'src'));
+
+  await _updateJSON(
+    path.resolve(projectBasePath, 'package.json'),
+    projectContent.project
+  );
+
+  await Promise.all(
+    projectContent.collections.map(collection =>
+      _writeProjectCollection(
+        path.resolve(projectBasePath, 'src', collection.slug),
+        collection
+      )
+    )
+  );
+};
+
+module.exports = writeProjectContent;
