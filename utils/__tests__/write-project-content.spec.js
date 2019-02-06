224401913a3fee316020bff7dda48b7eb91cc209
Pablo Molina <contact@pablomolina.me>
2019-02-06 19:56:13 +0100

    Add assertions for writeProjectContent parameters

--- utils/__tests__/write-project-content.spec.js
+++ utils/__tests__/write-project-content.spec.js
@@ -1,10 +1,21 @@
 const getProjectContent = require('../get-project-content');
 const getTestFixtures = require('../get-test-fixtures');
+const path = require('path');
 const tmp = require('tmp');
 const writeProjectContent = require('../write-project-content');
 
 describe('utils/write-project-content', () => {
   getTestFixtures().forEach(projectPath => {
+    it('needs a valid projectBasePath parameter', () => {
+      expect(writeProjectContent(null, null)).rejects.toMatchSnapshot();
+    });
+
+    it('needs a valid projectContent parameter', () => {
+      expect(
+        writeProjectContent(`${path.sep}tmp`, null)
+      ).rejects.toMatchSnapshot();
+    });
+
     it('writes a project inside a given path', async () => {
       const tmpDir = tmp.dirSync({ unsafeCleanup: true });
       const projectContent = getProjectContent(projectPath);
