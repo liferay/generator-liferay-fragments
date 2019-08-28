f62c436d0441d8382a283df41104c8360435281b
Pablo Molina <contact@pablomolina.me>
2019-08-28 11:15:31 +0200

    Show better error messages during import

--- generators/import/import.js
+++ generators/import/import.js
@@ -24,20 +24,236 @@ const DEFAULT_FRAGMENT_TYPE = FRAGMENT_TYPES.section;
 async function importProject(groupId, project) {
   log('Importing project', { data: project.project.name, newLine: true });
 
-  try {
-    await Promise.all(
-      project.collections.map(async collection => {
-        await _importCollection(groupId, collection);
-      })
-    );
+  // Show loading
 
-    log('Project sent successfully', {
-      newLine: true,
-      level: LOG_LEVEL.success
-    });
-  } catch (error) {
-    log('Project sent with errors', { newLine: true, level: LOG_LEVEL.error });
-  }
+  /**
+   * @type {Array<import('../../types/index').ICollectionRequest>}
+   */
+  let collectionRequests = [];
+
+  /**
+   * @type {Array<import('../../types/index').IFragmentRequest>}
+   */
+  let fragmentRequests = [];
+
+  collectionRequests = project.collections.map(collection => {
+    /**
+     * @type {import('../../types/index').ICollectionRequest}
+     */
+    const collectionRequest = {
+      collection,
+      existingCollection: undefined,
+      error: undefined,
+      status: 'pending',
+      promise: undefined
+    };
+
+    collectionRequest.promise = _importCollection(groupId, collection)
+      .then(existingCollection => {
+        collectionRequest.existingCollection = existingCollection;
+        collectionRequest.status = 'success';
+      })
+      .catch(error => {
+        collectionRequest.error = error;
+        collectionRequest.status = 'error';
+      });
+
+    return collectionRequest;
+  });
+
+  await Promise.all(
+    collectionRequests.map(collectionRequest => collectionRequest.promise)
+  );
+
+  fragmentRequests = collectionRequests
+    .map(collectionRequest =>
+      collectionRequest.collection.fragments.map(fragment => {
+        /**
+         * @type {import('../../types/index').IFragmentRequest}
+         */
+        const fragmentRequest = {
+          collection: collectionRequest.collection,
+          fragment,
+          existingFragment: undefined,
+          promise: undefined,
+          error: undefined,
+          status: 'pending'
+        };
+
+        if (
+          collectionRequest.status === 'success' &&
+          collectionRequest.existingCollection
+        ) {
+          fragmentRequest.promise = _importFragment(
+            groupId,
+            collectionRequest.existingCollection,
+            fragment
+          )
+            .then(([status, existingFragment]) => {
+              fragmentRequest.status = status;
+              fragmentRequest.existingFragment = existingFragment;
+            })
+            .catch(error => {
+              fragmentRequest.status = 'error';
+              fragmentRequest.error = error;
+            });
+        } else {
+          fragmentRequest.promise = Promise.resolve();
+          fragmentRequest.status = 'ignored';
+        }
+
+        return fragmentRequest;
+      })
+    )
+    .reduce((a, b) => [...a, ...b], []);
+
+  await Promise.all(
+    fragmentRequests.map(fragmentRequest => fragmentRequest.promise)
+  );
+
+  _logImportSummary(collectionRequests, fragmentRequests);
+  _logImportErrors(collectionRequests, fragmentRequests);
+}
+
+/**
+ * @param {Array<import('../../types/index').ICollectionRequest>} collectionRequests
+ * @param {Array<import('../../types/index').IFragmentRequest>} fragmentRequests
+ */
+function _logImportSummary(collectionRequests, fragmentRequests) {
+  const addedCount = fragmentRequests.filter(
+    fragmentRequest => fragmentRequest.status === 'added'
+  ).length;
+
+  const updatedCount = fragmentRequests.filter(
+    fragmentRequest => fragmentRequest.status === 'updated'
+  ).length;
+
+  const upToDateCount = fragmentRequests.filter(
+    fragmentRequest => fragmentRequest.status === 'upToDate'
+  ).length;
+
+  const ignoredCount = fragmentRequests.filter(
+    fragmentRequest => fragmentRequest.status === 'ignored'
+  ).length;
+
+  const errorCount = fragmentRequests.filter(
+    fragmentRequest => fragmentRequest.status === 'error'
+  ).length;
+
+  /**
+   * @param {number} count
+   * @param {string} message
+   * @param {'LOG_LEVEL_ERROR'|'LOG_LEVEL_SUCCESS'} [level='LOG_LEVEL_SUCCESS']
+   */
+  const statusLog = (count, message, level = 'LOG_LEVEL_SUCCESS') => {
+    let noun = fragmentRequests.length === 1 ? 'fragment' : 'fragments';
+    let verb = count === 1 ? 'was' : 'were';
+
+    if (count > 0) {
+      log(
+        `${count} of ${fragmentRequests.length} ${noun} ${verb} ${message}.`,
+        { level }
+      );
+    }
+  };
+
+  log('');
+  statusLog(addedCount, 'added successfully');
+  statusLog(updatedCount, 'updated successfully');
+  statusLog(upToDateCount, 'already up to date');
+
+  statusLog(
+    ignoredCount,
+    'ignored due to collection errors',
+    'LOG_LEVEL_ERROR'
+  );
+
+  statusLog(errorCount, 'ignored due to fragment errors', 'LOG_LEVEL_ERROR');
+}
+
+/**
+ * @param {Array<import('../../types/index').ICollectionRequest>} collectionRequests
+ * @param {Array<import('../../types/index').IFragmentRequest>} fragmentRequests
+ */
+function _logImportErrors(collectionRequests, fragmentRequests) {
+  const sortedFragmentRequests = [...fragmentRequests].sort((a, b) => {
+    if (a.collection.slug < b.collection.slug) {
+      return -1;
+    }
+
+    if (a.collection.slug > b.collection.slug) {
+      return 1;
+    }
+
+    if (a.fragment.slug < b.fragment.slug) {
+      return -1;
+    }
+
+    if (a.fragment.slug > b.fragment.slug) {
+      return 1;
+    }
+
+    return 0;
+  });
+
+  collectionRequests.forEach(collectionRequest => {
+    const name =
+      collectionRequest.collection.metadata.name ||
+      collectionRequest.collection.slug;
+
+    log('');
+    if (collectionRequest.status === 'success') {
+      log(`✔ Collection ${name}`, { level: LOG_LEVEL.success });
+    } else {
+      log(`✘ Collection ${name} was not imported`, { level: LOG_LEVEL.error });
+
+      if (collectionRequest.error) {
+        log(collectionRequest.error.toString());
+      }
+    }
+
+    sortedFragmentRequests
+      .filter(
+        fragmentRequest =>
+          fragmentRequest.collection.slug === collectionRequest.collection.slug
+      )
+      .forEach(fragmentRequest => {
+        if (fragmentRequest.status === 'error') {
+          log(
+            `✘ Fragment ${fragmentRequest.fragment.metadata.name ||
+              fragmentRequest.fragment.slug} was not imported`,
+            { level: LOG_LEVEL.error, indent: true }
+          );
+
+          if (fragmentRequest.error) {
+            log(fragmentRequest.error.toString(), { indent: true });
+          }
+        } else if (fragmentRequest.status === 'ignored') {
+          log(`↷ Fragment ${fragmentRequest.fragment.metadata.name} ignored`, {
+            level: LOG_LEVEL.error,
+            indent: true
+          });
+        } else if (fragmentRequest.status === 'added') {
+          log(`🞧 Fragment ${fragmentRequest.fragment.metadata.name} added`, {
+            level: LOG_LEVEL.success,
+            indent: true
+          });
+        } else if (fragmentRequest.status === 'updated') {
+          log(`⭮ Fragment ${fragmentRequest.fragment.metadata.name} updated`, {
+            level: LOG_LEVEL.success,
+            indent: true
+          });
+        } else {
+          log(
+            `✔ Fragment ${fragmentRequest.fragment.metadata.name} up-to-date`,
+            {
+              level: LOG_LEVEL.success,
+              indent: true
+            }
+          );
+        }
+      });
+  });
 }
 
 /**
@@ -76,10 +292,9 @@ function _getFragmentTypeId(type) {
  * Imports a collection to server
  * @param {string} groupId
  * @param {import('../../types/index').ICollection} collection
+ * @return {Promise<import('../../types/index').IServerCollection | undefined>}
  */
 async function _importCollection(groupId, collection) {
-  log('Importing collection', { data: collection.metadata.name });
-
   const { name, description } = collection.metadata;
   const { slug } = collection;
 
@@ -93,15 +308,7 @@ async function _importCollection(groupId, collection) {
     existingCollection = await _getExistingCollection(groupId, collection);
   }
 
-  if (existingCollection) {
-    await Promise.all(
-      collection.fragments.map(async fragment => {
-        if (existingCollection) {
-          await _importFragment(groupId, existingCollection, fragment);
-        }
-      })
-    );
-  }
+  return existingCollection;
 }
 
 /**
@@ -109,6 +316,7 @@ async function _importCollection(groupId, collection) {
  * @param {string} groupId Group ID
  * @param {import('../../types/index').IServerCollection} existingCollection
  * @param {import('../../types/index').IFragment} fragment
+ * @return {Promise<[import('../../types/index').IFragmentRequestStatus, import('../../types/index').IServerFragment | undefined]>}
  */
 async function _importFragment(groupId, existingCollection, fragment) {
   const { fragmentCollectionId } = existingCollection;
@@ -118,13 +326,13 @@ async function _importFragment(groupId, existingCollection, fragment) {
   const fragmentEntryKey = fragment.slug;
   const status = 0;
 
-  let existingFragment = await _getExistingFragment(
-    groupId,
-    existingCollection,
-    fragment
-  );
-
   try {
+    let existingFragment = await _getExistingFragment(
+      groupId,
+      existingCollection,
+      fragment
+    );
+
     if (existingFragment && _fragmentHasChanges(existingFragment, fragment)) {
       await api.updateFragmentEntry(existingFragment.fragmentEntryId, {
         status,
@@ -135,34 +343,30 @@ async function _importFragment(groupId, existingCollection, fragment) {
         configuration
       });
 
-      log('Updated', { data: fragment.metadata.name });
-    } else if (existingFragment) {
-      log('Up-to-date', { data: fragment.metadata.name });
-    } else {
-      existingFragment = await api.addFragmentEntry(
-        groupId,
-        fragmentCollectionId,
-        fragmentEntryKey,
-        {
-          status,
-          name,
-          type,
-          html,
-          css,
-          js,
-          configuration
-        }
-      );
-
-      log('Added', { data: fragment.metadata.name, level: LOG_LEVEL.success });
+      return ['updated', existingFragment];
     }
-  } catch (error) {
-    log('Error', {
-      data: fragment.metadata.name,
-      description: error.toString(),
-      level: LOG_LEVEL.error
-    });
 
+    if (existingFragment) {
+      return ['upToDate', existingFragment];
+    }
+
+    existingFragment = await api.addFragmentEntry(
+      groupId,
+      fragmentCollectionId,
+      fragmentEntryKey,
+      {
+        status,
+        name,
+        type,
+        html,
+        css,
+        js,
+        configuration
+      }
+    );
+
+    return ['added', existingFragment];
+  } catch (error) {
     throw error;
   }
 }
