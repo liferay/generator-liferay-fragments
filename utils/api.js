8d66d28d23ddae2552b9bfdbad6cd8f834978def
Pavel Savinov <pavel.savinov@liferay.com>
2020-04-15 08:45:42 +0200

    LPS-111817 API to render page definition and composition preview

--- utils/api.js
+++ utils/api.js
@@ -494,6 +494,16 @@ const api = {
 
   /**
    * @param {string} groupId
+   * @param {object} definition
+   */
+  async renderCompositionPreview(groupId, definition) {
+    return this.renderPageDefinitionPreview(groupId, {
+      pageElement: definition
+    });
+  },
+
+  /**
+   * @param {string} groupId
    * @param {string} html
    * @param {string} css
    * @param {string} js
@@ -514,6 +524,25 @@ const api = {
       },
       {
         headers: { Authorization: `Bearer ${this._oauthToken.accessToken}` }
+      }
+    );
+  },
+
+  /**
+   * @param {string} groupId
+   * @param {object} definition
+   */
+  async renderPageDefinitionPreview(groupId, definition) {
+    return this.request(
+      'POST',
+      `/o/headless-delivery/v1.0/sites/${groupId}/page-definitions/preview`,
+      {
+        body: definition,
+        headers: {
+          Accept: 'text/html',
+          Authorization: `Basic ${this._basicAuthToken}`
+        },
+        json: true
       }
     );
   }
