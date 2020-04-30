03be8ae8852c4ef0920c140321a28f5b27adc55e
Sandro Chinea <sandrochinea@gmail.com>
2020-04-30 13:53:30 +0100

    LPS-112177 Added tests for compress

--- generators/compress/__tests__/compress.spec.js
+++ generators/compress/__tests__/compress.spec.js
@@ -1,5 +1,63 @@
+const fs = require('fs');
+const path = require('path');
+const YeomanTest = require('yeoman-test');
+const AdmZip = require('adm-zip');
+
 describe('compress', () => {
-  test.todo('generates a zip file');
-  test.todo('appends deployment descriptor');
-  test.todo('appends all src files');
+  it('generates a zip file', () =>
+    YeomanTest.run(path.join(__dirname, '..')).then(projectPath => {
+      const zip = new AdmZip(
+        path.join(projectPath, 'build/liferay-fragments.zip')
+      );
+
+      expect(zip).not.toBe(null);
+    }));
+
+  it('appends deployment descriptor', () =>
+    YeomanTest.run(path.join(__dirname, '..'))
+      .withOptions({ addDeploymentDescriptor: true })
+      .withOptions({ companyWebId: '1' })
+      .withOptions({ groupKey: '2' })
+      .then(projectPath => {
+        const zip = new AdmZip(
+          path.join(projectPath, 'build/liferay-fragments.zip')
+        );
+
+        const descriptor = JSON.parse(
+          zip.readAsText('liferay-deploy-fragments.json')
+        );
+
+        expect(descriptor).toMatchObject({
+          companyWebId: '1',
+          groupKey: '2'
+        });
+      }));
+
+  it('appends all src files', () => {
+    const fragmentFolderPath = 'collection/fragment';
+    const fragmentFiles = ['index.html', 'main.js', 'styles.css'];
+
+    return YeomanTest.run(path.join(__dirname, '..'))
+      .inTmpDir(function(dir) {
+        fs.mkdirSync(path.join(dir, 'src'));
+        fs.mkdirSync(path.join(dir, 'src/collection'));
+        fs.mkdirSync(path.join(dir, 'src/collection/fragment'));
+
+        const composedPath = path.join(dir, 'src', fragmentFolderPath);
+        fragmentFiles.forEach(file =>
+          fs.writeFileSync(path.join(composedPath, file), '')
+        );
+      })
+      .then(projectPath => {
+        const zip = new AdmZip(
+          path.join(projectPath, 'build/liferay-fragments.zip')
+        );
+
+        fragmentFiles.forEach(file => {
+          expect(
+            zip.getEntry(path.posix.join(fragmentFolderPath, file))
+          ).not.toBe(null);
+        });
+      });
+  });
 });
--- types/yeoman-test.d.ts
+++ types/yeoman-test.d.ts
@@ -1,6 +1,7 @@
 declare module 'yeoman-test' {
   class YeomanTest {
     static run(path: string): typeof YeomanTest;
+    static inTmpDir(callback: (dir: string) => void): typeof YeomanTest;
     static withOptions(options: { [key: string]: any }): typeof YeomanTest;
     static withPrompts(prompts: { [key: string]: any }): typeof YeomanTest;
     static then(callback: (projectPath: string) => void): typeof YeomanTest;
