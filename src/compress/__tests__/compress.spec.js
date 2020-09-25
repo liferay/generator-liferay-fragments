2c1f6ecb5c880b179f5630ed055979a6668e153b
Pablo Molina <contact@pablomolina.me>
2020-09-25 13:14:32 +0200

    test: Simplify compress tests and use real content (LPS-119618)

--- src/compress/__tests__/compress.spec.js
+++ src/compress/__tests__/compress.spec.js
@@ -1,63 +1,18 @@
-const AdmZip = require('adm-zip');
-const fs = require('fs');
 const path = require('path');
-const YeomanTest = require('yeoman-test');
+
+const getTestFixtures = require('../../utils/get-test-fixtures');
+const {
+  default: getProjectContent,
+} = require('../../utils/project-content/get-project-content');
+const { default: compress } = require('../compress');
 
 describe('compress', () => {
-  it('generates a zip file', () =>
-    YeomanTest.run(path.join(__dirname, '..')).then((projectPath) => {
-      const zip = new AdmZip(
-        path.join(projectPath, 'build/liferay-fragments.zip')
-      );
+  getTestFixtures().forEach((fixturePath) => {
+    const projectContent = getProjectContent(fixturePath);
 
-      expect(zip).not.toBe(null);
-    }));
-
-  it('appends deployment descriptor', () =>
-    YeomanTest.run(path.join(__dirname, '..'))
-      .withOptions({ addDeploymentDescriptor: true })
-      .withOptions({ companyWebId: '1' })
-      .withOptions({ groupKey: '2' })
-      .then((projectPath) => {
-        const zip = new AdmZip(
-          path.join(projectPath, 'build/liferay-fragments.zip')
-        );
-
-        const descriptor = JSON.parse(
-          zip.readAsText('liferay-deploy-fragments.json')
-        );
-
-        expect(descriptor).toMatchObject({
-          companyWebId: '1',
-          groupKey: '2',
-        });
+    it(`generates a zip for ${path.basename(fixturePath)}`, () =>
+      compress(projectContent, {}).then((zip) => {
+        expect(Object.keys(zip.files).length).not.toBe(0);
       }));
-
-  it('appends all src files', () => {
-    const fragmentFolderPath = 'collection/fragment';
-    const fragmentFiles = ['index.html', 'main.js', 'styles.css'];
-
-    return YeomanTest.run(path.join(__dirname, '..'))
-      .inTmpDir((dir) => {
-        fs.mkdirSync(path.join(dir, 'src'));
-        fs.mkdirSync(path.join(dir, 'src/collection'));
-        fs.mkdirSync(path.join(dir, 'src/collection/fragment'));
-
-        const composedPath = path.join(dir, 'src', fragmentFolderPath);
-        fragmentFiles.forEach((file) =>
-          fs.writeFileSync(path.join(composedPath, file), '')
-        );
-      })
-      .then((projectPath) => {
-        const zip = new AdmZip(
-          path.join(projectPath, 'build/liferay-fragments.zip')
-        );
-
-        fragmentFiles.forEach((file) => {
-          expect(
-            zip.getEntry(path.posix.join(fragmentFolderPath, file))
-          ).not.toBe(null);
-        });
-      });
   });
 });
