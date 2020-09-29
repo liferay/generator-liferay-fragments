211d12c630cc0661cc8167772dc1ecb35cfbac11
Pablo Molina <contact@pablomolina.me>
2020-09-29 08:46:50 +0200

    test: Remove snapshots from get-project-content (LPS-119618)

--- src/utils/__tests__/get-project-content.spec.js
+++ src/utils/__tests__/get-project-content.spec.js
@@ -12,8 +12,43 @@ describe('utils/get-project-content', () => {
     it('matches a project structure', () => {
       const projectContent = getProjectContent(projectPath);
 
-      projectContent.basePath = '';
-      expect(projectContent).toMatchSnapshot();
+      expect(projectContent).toEqual(
+        expect.objectContaining({
+          basePath: projectPath,
+
+          project: expect.objectContaining({
+            name: expect.any(String),
+          }),
+
+          collections: expect.arrayContaining([
+            expect.objectContaining({
+              slug: expect.any(String),
+              fragmentCollectionId: expect.any(String),
+
+              metadata: expect.objectContaining({
+                name: expect.any(String),
+                description: expect.any(String),
+              }),
+
+              fragments: expect.arrayContaining([
+                expect.objectContaining({
+                  slug: expect.any(String),
+                  html: expect.any(String),
+                  js: expect.any(String),
+                  css: expect.any(String),
+
+                  metadata: expect.objectContaining({
+                    htmlPath: expect.any(String),
+                    jsPath: expect.any(String),
+                    cssPath: expect.any(String),
+                    name: expect.any(String),
+                  }),
+                }),
+              ]),
+            }),
+          ]),
+        })
+      );
     });
   });
 });
