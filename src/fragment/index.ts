0f55fb61b4ac6fdac0f37c067e66dc7a87d33b29
Pablo Molina <contact@pablomolina.me>
2020-10-19 08:00:17 +0200

    refactor: Inline FragmentGenerator private methods (LPS-116692)

--- src/fragment/index.ts
+++ src/fragment/index.ts
@@ -26,9 +26,74 @@ import { getCollectionChoices } from '../utils/get-collection-choices';
 
 export default class FragmentGenerator extends CustomGenerator {
   async prompting(): Promise<void> {
-    await this._askLiferayVersion();
-    await this._askFragmentData();
-    await this._askCollection();
+    const previousMinLiferayVersion = this.getValue(MIN_LIFERAY_VERSION_VAR);
+
+    await this.ask({
+      type: 'input',
+      name: MIN_LIFERAY_VERSION_VAR,
+      message: MIN_LIFERAY_VERSION_MESSAGE,
+      validate: (version) =>
+        semver.valid(version)
+          ? true
+          : MIN_LIFERAY_VERSION_MESSAGE_ERROR_MESSAGE,
+      when: !this.hasValue(MIN_LIFERAY_VERSION_VAR),
+    });
+
+    if (!previousMinLiferayVersion) {
+      this.config.set(
+        MIN_LIFERAY_VERSION_VAR,
+        `${this.getValue(MIN_LIFERAY_VERSION_VAR)}`
+      );
+    }
+
+    this.setDefaultValue(
+      DATA_LFR_SUPPORTED,
+
+      // @ts-ignore
+
+      semver.gte(
+        `${this.getValue(MIN_LIFERAY_VERSION_VAR)}`,
+        DATA_LFR_SUPPORTED_MIN_VERSION
+      )
+    );
+
+    await this.ask([
+      {
+        type: 'input',
+        name: FRAGMENT_NAME_VAR,
+        message: FRAGMENT_NAME_MESSAGE,
+
+        /** @param {string} name */
+        validate: (name) =>
+          name ? true : FRAGMENT_NAME_NON_EMPTY_ERROR_MESSAGE,
+        when: !this.hasValue(FRAGMENT_NAME_VAR),
+      },
+      {
+        type: 'list',
+        name: FRAGMENT_TYPE_VAR,
+        message: FRAGMENT_TYPE_MESSAGE,
+        choices: FRAGMENT_TYPE_OPTIONS,
+        default: this.getValue(FRAGMENT_TYPE_DEFAULT),
+        when:
+          !this.hasValue(FRAGMENT_TYPE_VAR) &&
+          !this.getValue(DATA_LFR_SUPPORTED),
+      },
+    ]);
+
+    this.setDefaultValue(
+      FRAGMENT_SLUG_VAR,
+      voca.slugify(this.getValue(FRAGMENT_NAME_VAR))
+    );
+
+    this.setDefaultValue(FRAGMENT_TYPE_VAR, FRAGMENT_TYPE_DEFAULT);
+
+    await this.ask({
+      type: 'list',
+      name: FRAGMENT_COLLECTION_SLUG_VAR,
+      message: FRAGMENT_COLLECTION_SLUG_MESSAGE,
+      choices: getCollectionChoices(this.destinationRoot()),
+      when: !this.hasValue(FRAGMENT_COLLECTION_SLUG_VAR),
+    });
   }
 
   writing(): void {
@@ -63,80 +128,5 @@ export default class FragmentGenerator extends CustomGenerator {
         'configuration.json',
       ]);
     }
-  }
-
-  private async _askCollection() {
-    await this.ask({
-      type: 'list',
-      name: FRAGMENT_COLLECTION_SLUG_VAR,
-      message: FRAGMENT_COLLECTION_SLUG_MESSAGE,
-      choices: getCollectionChoices(this.destinationRoot()),
-      when: !this.hasValue(FRAGMENT_COLLECTION_SLUG_VAR),
-    });
-  }
-
-  private async _askFragmentData() {
-    await this.ask([
-      {
-        type: 'input',
-        name: FRAGMENT_NAME_VAR,
-        message: FRAGMENT_NAME_MESSAGE,
-
-        /** @param {string} name */
-        validate: (name) =>
-          name ? true : FRAGMENT_NAME_NON_EMPTY_ERROR_MESSAGE,
-        when: !this.hasValue(FRAGMENT_NAME_VAR),
-      },
-      {
-        type: 'list',
-        name: FRAGMENT_TYPE_VAR,
-        message: FRAGMENT_TYPE_MESSAGE,
-        choices: FRAGMENT_TYPE_OPTIONS,
-        default: this.getValue(FRAGMENT_TYPE_DEFAULT),
-        when:
-          !this.hasValue(FRAGMENT_TYPE_VAR) &&
-          !this.getValue(DATA_LFR_SUPPORTED),
-      },
-    ]);
-
-    this.setDefaultValue(
-      FRAGMENT_SLUG_VAR,
-      voca.slugify(this.getValue(FRAGMENT_NAME_VAR))
-    );
-
-    this.setDefaultValue(FRAGMENT_TYPE_VAR, FRAGMENT_TYPE_DEFAULT);
-  }
-
-  private async _askLiferayVersion() {
-    const previousMinLiferayVersion = this.getValue(MIN_LIFERAY_VERSION_VAR);
-
-    await this.ask({
-      type: 'input',
-      name: MIN_LIFERAY_VERSION_VAR,
-      message: MIN_LIFERAY_VERSION_MESSAGE,
-      validate: (version) =>
-        semver.valid(version)
-          ? true
-          : MIN_LIFERAY_VERSION_MESSAGE_ERROR_MESSAGE,
-      when: !this.hasValue(MIN_LIFERAY_VERSION_VAR),
-    });
-
-    if (!previousMinLiferayVersion) {
-      this.config.set(
-        MIN_LIFERAY_VERSION_VAR,
-        `${this.getValue(MIN_LIFERAY_VERSION_VAR)}`
-      );
-    }
-
-    this.setDefaultValue(
-      DATA_LFR_SUPPORTED,
-
-      // @ts-ignore
-
-      semver.gte(
-        `${this.getValue(MIN_LIFERAY_VERSION_VAR)}`,
-        DATA_LFR_SUPPORTED_MIN_VERSION
-      )
-    );
   }
 }
