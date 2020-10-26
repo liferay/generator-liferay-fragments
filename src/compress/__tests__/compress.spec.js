d9d883e4eabbcb42278cc309683877f7151fb97d
Pablo Molina <contact@pablomolina.me>
2020-10-26 13:34:54 +0100

    test: Compare zip content when testing compress

--- src/compress/__tests__/compress.spec.js
+++ src/compress/__tests__/compress.spec.js
@@ -1,4 +1,7 @@
+const fs = require('fs');
+const mkdirp = require('mkdirp');
 const path = require('path');
+const tmp = require('tmp');
 
 const getTestFixtures = require('../../utils/get-test-fixtures');
 const {
@@ -10,9 +13,43 @@ describe('compress', () => {
   getTestFixtures().forEach((fixturePath) => {
     const projectContent = getProjectContent(fixturePath);
 
-    it(`generates a zip for ${path.basename(fixturePath)}`, () =>
-      compress(projectContent, {}).then((zip) => {
-        expect(Object.keys(zip.files).length).not.toBe(0);
-      }));
+    it(`generates a zip for ${path.basename(fixturePath)}`, async () => {
+      const zip = await compress(projectContent, {});
+      const tmpDir = tmp.dirSync({ unsafeCleanup: true });
+      const files = Object.entries(zip.files).filter(([, data]) => !data.dir);
+
+      for (const [key, data] of files) {
+        const filePath = path.resolve(tmpDir.name, key);
+        const directoryPath = path.dirname(filePath);
+
+        if (!fs.existsSync(directoryPath)) {
+          mkdirp.sync(directoryPath, { recursive: true });
+        }
+
+        fs.writeFileSync(filePath, await data.async('nodebuffer'));
+      }
+
+      const compressedProjectContent = getProjectContent(tmpDir.name);
+      tmpDir.removeCallback();
+
+      delete compressedProjectContent.basePath;
+      delete compressedProjectContent.unknownFiles;
+      delete projectContent.basePath;
+      delete projectContent.unknownFiles;
+
+      compressedProjectContent.collections.forEach((collection) => {
+        collection.fragments.forEach((fragment) => {
+          delete fragment.unknownFiles;
+        });
+      });
+
+      projectContent.collections.forEach((collection) => {
+        collection.fragments.forEach((fragment) => {
+          delete fragment.unknownFiles;
+        });
+      });
+
+      expect(compressedProjectContent).toEqual(projectContent);
+    });
   });
 });
