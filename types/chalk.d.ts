ba6987cf734f2eb4932c2c3831bc0f83be07ec1a
Pablo Molina <contact@pablomolina.me>
2021-01-12 11:44:35 +0100

    build: Update chalk@^4.1.0

--- types/chalk.d.ts
+++
@@ -1,6 +0,0 @@
-declare module 'chalk' {
-  export function reset(message: string): string;
-  export function bold(message: string): string;
-  export function green(message: string): string;
-  export function red(message: string): string;
-}
