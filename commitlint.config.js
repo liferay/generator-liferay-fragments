5a71d72986e4e7c8c37cd1ee3e391b787a31b0cb
Pablo Molina <contact@pablomolina.me>
2021-06-23 09:31:50 +0200

    chore: Update commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,5 +2,25 @@ module.exports = {
   extends: ['@commitlint/config-angular'],
   rules: {
     'subject-case': [2, 'always', 'sentence-case'],
+
+    // https://github.com/conventional-changelog/commitlint/tree/master/@commitlint/config-conventional#type-enum
+
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build',
+        'chore',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'revert',
+        'style',
+        'test',
+      ],
+    ],
   },
 };
