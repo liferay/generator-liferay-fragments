ae5fb6fee827f020c31f21972f0051dedf90367d
Pablo Molina <contact@pablomolina.me>
2022-02-03 17:21:52 +0100

    feat: Add SASS build step (LPS-146880)

--- src/utils/project-content/build-project-content.ts
+++ src/utils/project-content/build-project-content.ts
@@ -1,6 +1,7 @@
 import execa from 'execa';
 import fs from 'fs';
 import path from 'path';
+import sass from 'sass';
 
 import { IProject } from '../../../types';
 import { BUNDLER_OUTPUT_DIR } from '../constants';
@@ -12,6 +13,55 @@ import writeProjectContent from './write-project-content';
 export const buildProjectContent = async (
   projectContent: IProject
 ): Promise<IProject> => {
+  return await applySASS(await applyNPMBundler(projectContent));
+};
+
+async function applySASS(projectContent: IProject): Promise<IProject> {
+  const tmpDir = createTemporaryDirectory();
+  await writeProjectContent(tmpDir.name, projectContent);
+  const builtProjectContent = getProjectContent(tmpDir.name);
+
+  for (const collection of builtProjectContent.collections) {
+    for (const fragment of collection.fragments) {
+      const cssPath = fragment.metadata.cssPath.toLowerCase();
+
+      if (cssPath.endsWith('.scss') || cssPath.endsWith('.sass')) {
+        fragment.css = (
+          await sass.compileAsync(
+            path.resolve(
+              builtProjectContent.basePath,
+              'src',
+              collection.slug,
+              fragment.slug,
+              fragment.metadata.cssPath
+            ),
+            {
+              style: 'compressed',
+              verbose: true,
+            }
+          )
+        ).css;
+
+        fragment.metadata.cssPath = fragment.metadata.cssPath.replace(
+          /\.s(a|c)ss$/,
+          '.css'
+        );
+
+        fragment.unknownFiles = fragment.unknownFiles.filter((unknownFile) => {
+          const filePath = unknownFile.filePath.toLowerCase();
+
+          return !filePath.endsWith('.scss') && !filePath.endsWith('.sass');
+        });
+      }
+    }
+  }
+
+  tmpDir.removeCallback();
+
+  return builtProjectContent;
+}
+
+async function applyNPMBundler(projectContent: IProject): Promise<IProject> {
   const projectExports = getProjectExports(projectContent);
 
   const hasBundlerConfig = projectContent.unknownFiles.some(
@@ -55,4 +105,4 @@ export const buildProjectContent = async (
   }
 
   return builtProjectContent;
-};
+}
