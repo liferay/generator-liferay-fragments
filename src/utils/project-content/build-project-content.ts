0dabc1ce3519a7e9eaa0942578918d845aabd87a
Pablo Molina <contact@pablomolina.me>
2022-02-10 09:29:58 +0100

    fix: Use sass binary (LPS-146880)

--- src/utils/project-content/build-project-content.ts
+++ src/utils/project-content/build-project-content.ts
@@ -1,7 +1,6 @@
 import execa from 'execa';
 import fs from 'fs';
 import path from 'path';
-import sass from 'sass';
 
 import { IProject } from '../../../types';
 import { BUNDLER_OUTPUT_DIR } from '../constants';
@@ -9,6 +8,10 @@ import { createTemporaryDirectory } from '../temporary';
 import getProjectContent from './get-project-content';
 import { getProjectExports } from './get-project-exports';
 import writeProjectContent from './write-project-content';
+
+const nodeModulesBinPath = path.normalize(
+  path.resolve(__dirname, '../../../node_modules/.bin')
+);
 
 export const buildProjectContent = async (
   projectContent: IProject
@@ -20,27 +23,60 @@ async function applySASS(projectContent: IProject): Promise<IProject> {
   const tmpDir = createTemporaryDirectory();
   await writeProjectContent(tmpDir.name, projectContent);
   const builtProjectContent = getProjectContent(tmpDir.name);
+  const sassBinaryPath = path.resolve(nodeModulesBinPath, 'sass');
 
   for (const collection of builtProjectContent.collections) {
     for (const fragment of collection.fragments) {
       const cssPath = fragment.metadata.cssPath.toLowerCase();
 
       if (cssPath.endsWith('.scss') || cssPath.endsWith('.sass')) {
-        fragment.css = (
-          await sass.compileAsync(
-            path.resolve(
-              builtProjectContent.basePath,
-              'src',
-              collection.slug,
-              fragment.slug,
-              fragment.metadata.cssPath
-            ),
-            {
-              style: 'compressed',
-              verbose: true,
-            }
-          )
-        ).css;
+        const absoluteFragmentPath = path.resolve(
+          builtProjectContent.basePath,
+          'src',
+          collection.slug,
+          fragment.slug
+        );
+
+        const absoluteCSSPath = path.resolve(
+          absoluteFragmentPath,
+          fragment.metadata.cssPath
+        );
+
+        const absoluteCSSDestinationPath = absoluteCSSPath.replace(
+          /\.s(a|c)ss$/,
+          '.css'
+        );
+
+        const loadPaths: string[] = [];
+
+        for (const loadPath of fragment.metadata.sass?.loadPaths || []) {
+          const absolutePath = path.isAbsolute(loadPath)
+            ? loadPath
+            : path.resolve(absoluteFragmentPath, loadPath);
+
+          if (fs.existsSync(absolutePath)) {
+            loadPaths.push(absolutePath);
+          }
+        }
+
+        await execa(
+          sassBinaryPath,
+          [
+            absoluteCSSPath,
+            absoluteCSSDestinationPath,
+            `--load-path=${path.dirname(absoluteCSSPath)}`,
+            ...loadPaths.map((sassLoadPath) => `--load-path=${sassLoadPath}`),
+          ],
+          {
+            cwd: builtProjectContent.basePath,
+          }
+        );
+
+        if (!fs.existsSync(absoluteCSSDestinationPath)) {
+          throw new Error('Build error');
+        }
+
+        fragment.css = fs.readFileSync(absoluteCSSDestinationPath, 'utf8');
 
         fragment.metadata.cssPath = fragment.metadata.cssPath.replace(
           /\.s(a|c)ss$/,
@@ -71,9 +107,7 @@ async function applyNPMBundler(projectContent: IProject): Promise<IProject> {
   let builtProjectContent = projectContent;
 
   const bundlerBinaryPath = path.resolve(
-    projectContent.basePath,
-    'node_modules',
-    '.bin',
+    nodeModulesBinPath,
     'liferay-npm-bundler'
   );
 
