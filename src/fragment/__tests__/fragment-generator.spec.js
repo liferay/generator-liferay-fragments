4ee88e83b88018738f99068af80f5aa8a4e40e9e
Pablo Molina <contact@pablomolina.me>
2022-06-06 15:48:12 +0200

    test: Update fragment creation tests (LPS-155442)

--- src/fragment/__tests__/fragment-generator.spec.js
+++ src/fragment/__tests__/fragment-generator.spec.js
@@ -20,23 +20,9 @@ function expectFiles(base, paths) {
 }
 
 describe('fragment-generator', () => {
-  it('generates a new fragment', () =>
-    YeomanTest.run(path.join(__dirname, '..'))
-      .withOptions({ minLiferayVersion: '7.2.0' })
-      .withOptions({ fragmentName: 'Sample Fragment' })
-      .withOptions({ fragmentType: 'section' })
-      .withOptions({ fragmentCollectionSlug: 'sample-collection' })
-      .then((projectPath) => {
-        expectFiles(
-          path.join(projectPath, 'src', 'sample-collection', 'sample-fragment'),
-          ['fragment.json', 'index.html', 'styles.css', 'main.js']
-        );
-      }));
-
   it('needs a name', () =>
     new Promise((resolve, reject) => {
       YeomanTest.run(path.join(__dirname, '..'))
-        .withOptions({ minLiferayVersion: '7.2.0' })
         .withOptions({ fragmentType: 'section' })
         .withOptions({ fragmentCollectionSlug: 'sample-collection' })
         .then(reject)
@@ -46,23 +32,35 @@ describe('fragment-generator', () => {
   it('needs a collection', () =>
     new Promise((resolve, reject) => {
       YeomanTest.run(path.join(__dirname, '..'))
-        .withOptions({ minLiferayVersion: '7.2.0' })
         .withOptions({ fragmentName: 'Sample Fragment' })
-        .withOptions({ fragmentType: 'section' })
+        .withOptions({ fragmentType: 'component' })
         .then(reject)
         .catch(resolve);
     }));
 
-  it('generates new features if supported', () =>
-    YeomanTest.run(path.join(__dirname, '..'))
-      .withOptions({ minLiferayVersion: '7.3.3' })
-      .withOptions({ fragmentName: 'Sample Fragment' })
-      .withOptions({ fragmentType: 'section' })
-      .withOptions({ fragmentCollectionSlug: 'sample-collection' })
-      .then((projectPath) => {
-        expectFiles(
-          path.join(projectPath, 'src', 'sample-collection', 'sample-fragment'),
-          ['fragment.json', 'index.html', 'styles.css', 'main.js']
-        );
-      }));
+  test.each(['component', 'react', 'input'])(
+    'generates a %p fragment',
+    (fragmentType) =>
+      YeomanTest.run(path.join(__dirname, '..'))
+        .withOptions({ fragmentName: 'Sample Fragment' })
+        .withOptions({ fragmentType })
+        .withOptions({ fragmentCollectionSlug: 'sample-collection' })
+        .then((projectPath) => {
+          expectFiles(
+            path.join(
+              projectPath,
+              'src',
+              'sample-collection',
+              'sample-fragment'
+            ),
+            [
+              'configuration.json',
+              'fragment.json',
+              'index.html',
+              'styles.css',
+              'main.js',
+            ]
+          );
+        })
+  );
 });
